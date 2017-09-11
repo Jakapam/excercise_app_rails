c7a38c2a5ee09d7ac26804a8cbf134627648c9c4
bleavs <bleavitt@colgate.edu>
2017-09-11 15:35:55 -0400

    added exercise dummies for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ # Exercises
+
+  Exercise.new(name: "Bench Press",sets: 4, reps: 12, weight: 135)
+
+  Exercise.new(name: "Squats",sets: 4, reps: 10, weight: 135)
+
+  Exercise.new(name: "Shoulder Press",sets: 3, reps: 10, weight: 135)
+
+  Exercise.new(name: "Bicep Curls",sets: 5, reps: 8, weight: 135)
+
+  Exercise.new(name: "Deadlifts",sets: 4, reps: 6, weight: 135)
+
+
+ # # Routines
+ #
+ # Routine.create
+ # Routine.create
